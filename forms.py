87cd032debfd7e726508e66dac64956dfea658a2
emanuelmachado1983 <33380573+emanuelmachado1983@users.noreply.github.com>
2017-11-21 21:04:31 -0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -15,7 +15,26 @@ class RegistrarForm(LoginForm):
 class ProductosPorClienteForm(FlaskForm):
     cliente = StringField('Cliente', validators=[Required()])
     enviar = SubmitField('Buscar')
+    generarArchivo = SubmitField('Generar Archivo')
 
 class ClientesPorProductosForm(FlaskForm):
     producto = StringField('Producto', validators=[Required()])
     enviar = SubmitField('Buscar')
+    generarArchivo = SubmitField('Generar Archivo')
+
+
+class ProductosMasVendidosForm(FlaskForm):
+    generarArchivo = SubmitField('Generar Archivo')
+
+class ClientesMasCompradoresForm(FlaskForm):
+    generarArchivo = SubmitField('Generar Archivo')
+
+class GenerarNuevoUsuarioForm(FlaskForm):
+    usuario = StringField('Nombre de usuario', validators=[Required()])
+    password = StringField('Contraseña', validators=[Required()])
+    enviar = SubmitField('Aceptar')
+
+class CambiarClaveForm(FlaskForm):
+    password = PasswordField('Contraseña', validators=[Required()])
+    password_check = PasswordField('Verificar Contraseña', validators=[Required()])
+    enviar = SubmitField('Aceptar')
